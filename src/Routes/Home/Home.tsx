be41cdfdd4e54b4d63b5803500ea142e1c5f6336
Milan Hommet <milan.hommet@protonmail.com>
2024-06-12 15:02:59 +0200

    feat: :sparkles: Navbar, footer, tours content, tour details page, home page, 404 page

--- src/Routes/Home/Home.tsx
+++ src/Routes/Home/Home.tsx
@@ -1,14 +1,45 @@
 import React from "react";
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Home</h1>
-        <h1 className="text-3xl font-bold underline">Hello</h1>
+export default function Home() {
+  return (
+    <div className="flex flex-wrap">
+      <div className="w-full sm:w-8/12 mb-10">
+        <div className="container mx-auto h-full sm:p-10">
+          <header className="container px-4 lg:flex mt-10 items-center h-full lg:mt-0">
+            <div className="w-full">
+              <h1 className="text-4xl lg:text-6xl font-bold">
+                Discover the History of
+                <span className="text-orange-500"> DDay</span> Normandy.
+              </h1>
+              <div className="w-20 h-2 bg-orange-700 my-4"></div>
+              <p className="text-xl mb-10">
+                Experience a once in a lifetime opportunity to see the sites of
+                the famous World War II event, with our team of knowledgeable
+                guides.
+              </p>
+              <div className="flex gap-4">
+                <a
+                  href="/tour"
+                  className="bg-orange-700 text-white text-center text-2xl font-medium px-4 py-2 rounded-xl shadow border-white border-2 hover:bg-white hover:text-orange-700 hover:border-orange-700"
+                >
+                  Explore tours
+                </a>
+                <a
+                  href="/about"
+                  className="bg-white text-orange-700 text-2xl text-center font-medium px-4 py-2 rounded-xl shadow border-orange-700 border-2 hover:bg-orange-700 hover:text-white hover:border-white"
+                >
+                  About us
+                </a>
+              </div>
+            </div>
+          </header>
+        </div>
       </div>
-    );
-  }
+      <img
+        src="/img/utah.jpg"
+        alt="Beach of Normandy, France"
+        className="w-full h-48 object-cover sm:h-screen sm:w-4/12"
+      />
+    </div>
+  );
 }
-
-export default Home;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 
@@ -8,17 +8,26 @@ import Home from './Routes/Home/Home.tsx';
 import Tour from './Routes/Tour/Tour.tsx';
 import About from './Routes/About/About.tsx';
 import Contact from './Routes/Contact/Contact.tsx';
+import TourDetails from './Routes/Tour/TourDetails.tsx';
+import NotFound from './Routes/404.tsx';
+
+import Navbar from './Components/Navbar.tsx';
+import Footer from './Components/Footer.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tour" element={<Tour />} />
+        <Route path="/tour/:id" element={<TourDetails />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </Router>
   </React.StrictMode>
 );
