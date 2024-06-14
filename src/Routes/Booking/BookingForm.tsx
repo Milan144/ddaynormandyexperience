29f14f508005fe2db7826ea26ce88c39ca164392
Milan Hommet <milan.hommet@protonmail.com>
2024-06-14 12:29:12 +0200

    feat: :sparkles: page booking form

--- src/Routes/Booking/BookingForm.tsx
+++ src/Routes/Booking/BookingForm.tsx
@@ -1,18 +1,55 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { ToursData } from "../../Components/ToursData.ts";
+import emailjs from "emailjs-com";
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
 
 export default function BookingForm() {
-
   const { id } = useParams<{ id: string }>();
   if (id === undefined) {
     return <h1>404 page not found</h1>;
   }
   const tourTitle = ToursData[id].title;
-  console.log(tourTitle);
+
+  function createErrorNotification() {
+    return () => {
+      NotificationManager.error(
+        "Error while sending message",
+        "Error",
+        6000,
+        () => {
+          alert("callback");
+        }
+      );
+    };
+  }
+
+  function handleBooking(e: string | any) {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_BOOKING_TEMPLATE_ID,
+        e.target,
+        process.env.REACT_APP_USER_ID
+      )
+      .then(
+        (result) => {
+          window.location.href = "/booking/success";
+        },
+        (error) => {
+          createErrorNotification()();
+        }
+      );
+    e.target.reset();
+  }
 
   return (
     <div>
+      <NotificationContainer />
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
@@ -21,7 +58,8 @@ export default function BookingForm() {
           <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
             Want to book a tour ?
           </p>
-          <form action="#" className="space-y-8">
+          <form onSubmit={handleBooking} className="space-y-8">
+            {" "}
             <div>
               <label
                 htmlFor="subject"
@@ -31,6 +69,7 @@ export default function BookingForm() {
               </label>
               <input
                 type="text"
+                name="tour"
                 id="lastname"
                 className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 value={tourTitle}
@@ -47,6 +86,7 @@ export default function BookingForm() {
               </label>
               <input
                 type="text"
+                name="firstname"
                 id="firstname"
                 className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Your firstname"
@@ -63,6 +103,7 @@ export default function BookingForm() {
               <input
                 type="text"
                 id="lastname"
+                name="lastname"
                 className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Your lastname"
                 required
@@ -78,8 +119,57 @@ export default function BookingForm() {
               <input
                 type="email"
                 id="email"
+                name="email"
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Your email"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="phone"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Phone
+              </label>
+              <input
+                type="tel"
+                name="phone"
+                id="phone"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="Your phone number"
+                pattern="[0-9\s\-\(\)]*"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="date"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Date
+              </label>
+              <input
+                type="date"
+                id="date"
+                name="date"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="number"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Number of people
+              </label>
+              <input
+                type="number"
+                id="number"
+                name="number"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="Number of people"
                 required
               />
             </div>
@@ -88,11 +178,12 @@ export default function BookingForm() {
                 htmlFor="message"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
               >
-                Message
+                Note (optional)
               </label>
               <textarea
                 id="message"
                 rows={6}
+                name="message"
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Your message"
               ></textarea>
