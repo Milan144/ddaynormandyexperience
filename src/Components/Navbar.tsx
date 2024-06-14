a3a974ac814e0a855376b0e6aa69cc0ff3280366
Milan Hommet <milan.hommet@protonmail.com>
2024-06-14 16:06:04 +0200

    fix: :bug: fix mobile menu and put link on all tour card

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,39 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Navbar() {
-    return (
-        <header className="bg-gray-800">
-  <nav className="container mx-auto px-6 py-3">
-    <div className="flex items-center justify-between">
-      <div className="text-white font-bold text-xl">
-        <a href="/">Dday Normandy Experience</a>
-      </div>
-      <div className="hidden md:block">
-        <ul className="flex items-center space-x-8">
-          <li><a href="/" className="text-white">Home</a></li>
-          <li><a href="/about" className="text-white">About us</a></li>
-          <li><a href="/tour" className="text-white">Tours</a></li>
-          <li><a href="/contact" className="text-white">Contact us</a></li>
-        </ul>
-      </div>
-      <div className="md:hidden">
-        <button className="outline-none mobile-menu-button">
-          <svg className="w-6 h-6 text-white" x-show="!showMenu" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-            <path d="M4 6h16M4 12h16M4 18h16"></path>
-          </svg>
-        </button>
-      </div>
-    </div>
-    <div className="mobile-menu hidden md:hidden">
-      <ul className="mt-4 space-y-4">
-        <li><a href="/" className="block px-4 py-2 text-white bg-gray-900 rounded">Home</a></li>
-        <li><a href="/about" className="block px-4 py-2 text-white bg-gray-900 rounded">About us</a></li>
-        <li><a href="/tour" className="block px-4 py-2 text-white bg-gray-900 rounded">Tours</a></li>
-        <li><a href="/contact" className="block px-4 py-2 text-white bg-gray-900 rounded">Contact us</a></li>
-      </ul>
-    </div>
-    
-  </nav>
-        </header>
-    )
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenuToggle = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <header className="bg-gray-800">
+      <nav className="container mx-auto px-6 py-3">
+        <div className="flex items-center justify-between">
+          <div className="text-white font-bold text-xl">
+            <a href="/">Dday Normandy Experience</a>
+          </div>
+          <div className="hidden md:block">
+            <ul className="flex items-center space-x-8">
+              <li>
+                <a href="/" className="text-white">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="/about" className="text-white">
+                  About us
+                </a>
+              </li>
+              <li>
+                <a href="/tour" className="text-white">
+                  Tours
+                </a>
+              </li>
+              <li>
+                <a href="/contact" className="text-white">
+                  Contact us
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="md:hidden">
+            <button
+              className="outline-none mobile-menu-button"
+              onClick={handleMenuToggle}
+            >
+              <svg
+                className="w-6 h-6 text-white"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M4 6h16M4 12h16M4 18h16"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className={`mobile-menu ${showMenu ? "" : "hidden"} md:hidden`}>
+          <ul className="mt-4 space-y-4">
+            <li>
+              <a
+                href="/"
+                className="block px-4 py-2 text-white bg-gray-900 rounded"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="/about"
+                className="block px-4 py-2 text-white bg-gray-900 rounded"
+              >
+                About us
+              </a>
+            </li>
+            <li>
+              <a
+                href="/tour"
+                className="block px-4 py-2 text-white bg-gray-900 rounded"
+              >
+                Tours
+              </a>
+            </li>
+            <li>
+              <a
+                href="/contact"
+                className="block px-4 py-2 text-white bg-gray-900 rounded"
+              >
+                Contact us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
--- src/Routes/Tour/Tour.tsx
+++ src/Routes/Tour/Tour.tsx
@@ -13,9 +13,10 @@ class Tour extends React.Component {
         <section className="grid min-h-screen p-8 place-items-center">
           <div className="container grid grid-cols-1 gap-8 my-auto lg:grid-cols-2">
             {ToursData.map((tour, index) => (
-              <div
+              <a
                 key={index}
-                className="relative flex-col bg-clip-border rounded-xl bg-gray-100 text-gray-700 shadow-none grid gap-2 item sm:grid-cols-2"
+                href={`/tour/${index}`}
+                className="relative flex-col bg-clip-border rounded-xl bg-gray-100 text-gray-700 shadow-none grid gap-2 item sm:grid-cols-2 no-underline hover:no-underline"
               >
                 <div className="relative bg-clip-border rounded-xl overflow-hidden bg-white text-gray-700 shadow-lg m-0">
                   <img
@@ -28,17 +29,16 @@ class Tour extends React.Component {
                   <p className="block antialiased font-sans text-sm font-light leading-normal text-inherit mb-4">
                     Duration: {tour.duration}
                   </p>
-                  <a
-                    href={`/tour/${index}`}
+                  <div
                     className="block antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-blue-gray-900 mb-2 normal-case transition-colors hover:text-gray-700"
                   >
                     {tour.title}
-                  </a>
+                  </div>
                   <p className="block font-sans text-sm font-light leading-normal text-inherit mb-4">
                     {tour.overview}
                   </p>
                 </div>
-              </div>
+              </a>
             ))}
           </div>
         </section>
